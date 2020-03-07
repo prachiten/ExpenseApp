df59ec564b92a0b47a367001d4318f9feba3710e
prachiten <61093158+prachiten@users.noreply.github.com>
2020-03-06 23:04:02 -0800

    starting project

---
+++ ExpenseApp/FirstPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExpenseApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FirstPage : ContentPage
+    {
+        public FirstPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSetButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
