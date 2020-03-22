907e5abc48c75b6f7d1e9452ec1d336bb7aedc3b
Rosemary Delgado <33888379+rosemarymdelgado1@users.noreply.github.com>
2020-03-22 13:02:57 -0700

    Delete button modification

--- ExpenseApp/TransactionPage.xaml.cs
+++ ExpenseApp/TransactionPage.xaml.cs
@@ -50,18 +50,6 @@ namespace ExpenseApp
         {
             await Navigation.PopAsync();
         }
-        async void OnDeleteButtonClicked(object sender, EventArgs e)
-        {
-            var contents = BindingContext as Transaction;
-            if (contents == null)
-                return;
-            if (File.Exists(contents.Filename))
-            {
-                File.Delete(contents.Filename);
-            }
-            await Navigation.PopAsync();
-
-        }
 
         private void TransactionDatePicker_DateSelected(object sender, DateChangedEventArgs e)
         {
